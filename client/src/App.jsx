bd835138bfd8325ef25dfe74dc04e017aad88adc
harshana25 <harshanaheenatigala3@gmail.com>
2025-02-02 20:21:52 +0530

    Server Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/Sing-In" element={<SignIn />} />
-          <Route path="/Sing-Up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
